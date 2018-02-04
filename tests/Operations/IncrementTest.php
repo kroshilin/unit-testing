e7adbfffefd923a197580e5264f3884eeaab0067
Anton Kroshilin <kroshilin.a@afkxxx.com>
2018-02-04 13:43:46 +0200

    usage of @depends annotation

--- tests/Operations/IncrementTest.php
+++ tests/Operations/IncrementTest.php
@@ -9,13 +9,24 @@
 namespace CalcTest\Operations;
 
 use Calc\Operations\Incrementation;
+use Calc\Operations\OperationInterface;
 use PHPUnit\Framework\TestCase;
 
 class IncrementTest extends TestCase
 {
-    public function testOperationResult()
+    public function testCreateOperation()
     {
         $operation = new Incrementation();
+        $this->assertInstanceOf(OperationInterface::class, $operation);
+        return $operation;
+    }
+
+    /**
+     * @depends testCreateOperation
+     * @param Incrementation $operation
+     */
+    public function testOperationResult(Incrementation $operation)
+    {
         $operation->setArguments([5]);
         $result = $operation->run();
         $this->assertEquals(6, $result);
